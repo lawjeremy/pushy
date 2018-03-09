9bd3cf33ac5d8bfa86070877c207de037b26c4a6
Jeremy Law <jeremy@jeremylaW.ca>
2018-03-09 16:40:55 -0500

    transition from jquery to vanillajs

--- js/pushy.js
+++ js/pushy.js
@@ -3,32 +3,32 @@
 * https://github.com/christophery/pushy/
 * by Christopher Yee */
 
-(function ($) {
-	var pushy = $('.pushy'), //menu css class
-		body = $('body'),
-		container = $('#container'), //container css class
-		push = $('.push'), //css class to add pushy capability
+(function () {
+	var pushy = document.querySelector('.pushy'), //menu css class
+		body = document.querySelector('body'),
+		container = document.querySelector('#container'), //container css class
+		push = document.querySelector('.push'), //css class to add pushy capability
 		pushyLeft = 'pushy-left', //css class for left menu position
 		pushyOpenLeft = 'pushy-open-left', //css class when menu is open (left position)
 		pushyOpenRight = 'pushy-open-right', //css class when menu is open (right position)
-		siteOverlay = $('.site-overlay'), //site overlay
-		menuBtn = $('.menu-btn, .pushy-link'), //css classes to toggle the menu
-		menuBtnFocus = $('.menu-btn'), //css class to focus when menu is closed w/ esc key
-		menuLinkFocus = $(pushy.data('focus')), //focus on link when menu is open
+		siteOverlay = document.querySelector('.site-overlay'), //site overlay
+		menuBtn = document.querySelectorAll('.menu-btn, .pushy-link'), //css classes to toggle the menu
+		menuBtnFocus = document.querySelector('.menu-btn'), //css class to focus when menu is closed w/ esc key
+		menuLinkFocus = document.querySelector(pushy.getAttribute('data-focus')), //focus on link when menu is open
 		menuSpeed = 200, //jQuery fallback menu speed
-		menuWidth = pushy.width() + 'px', //jQuery fallback menu width
+		menuWidth = parseInt(getComputedStyle(pushy).width) + 'px', //jQuery fallback menu width
 		submenuClass = '.pushy-submenu',
 		submenuOpenClass = 'pushy-submenu-open',
 		submenuClosedClass = 'pushy-submenu-closed',
-		submenu = $(submenuClass);
-
+		submenu = document.querySelector(submenuClass);
+	
 	//close menu w/ esc key
-	$(document).keyup(function(e) {
+	document.addEventListener('keyup', function(e) {
 		//check if esc key is pressed
 		if (e.keyCode == 27) {
 
 			//check if menu is open
-			if( body.hasClass(pushyOpenLeft) || body.hasClass(pushyOpenRight) ){
+			if( body.classList.contains(pushyOpenLeft) || body.classList.contains(pushyOpenRight) ){
 				if(cssTransforms3d){
 					closePushy(); //close pushy
 				}else{
@@ -48,39 +48,43 @@
 
 	function togglePushy(){
 		//add class to body based on menu position
-		if( pushy.hasClass(pushyLeft) ){
-			body.toggleClass(pushyOpenLeft);
+		if( pushy.classList.contains(pushyLeft) ){
+			body.classList.toggle(pushyOpenLeft);
 		}else{
-			body.toggleClass(pushyOpenRight);
+			body.classList.toggle(pushyOpenRight);
 		}
 
 		//focus on link in menu after css transition ends
 		if(menuLinkFocus){
-			pushy.one('transitionend', function() {
+			pushy.addEventListener('transitionend', function(e) {
 				menuLinkFocus.focus();
-			});
+
+				//remove event listener to ensure event only fires once
+				e.target.removeEventListener(e.type, arguments.callee);
+					return;
+				});
 		}
 		
 	}
 
 	function closePushy(){
-		if( pushy.hasClass(pushyLeft) ){
-			body.removeClass(pushyOpenLeft);
+		if( pushy.classList.contains(pushyLeft) ){
+			body.classList.remove(pushyOpenLeft);
 		}else{
-			body.removeClass(pushyOpenRight);
+			body.classList.remove(pushyOpenRight);
 		}
 	}
 
 	function openPushyFallback(){
 		//animate menu position based on CSS class
-		if( pushy.hasClass(pushyLeft) ){
-			body.addClass(pushyOpenLeft);
+		if( pushy.classList.contains(pushyLeft) ){
+			body.classList.add(pushyOpenLeft);
 			pushy.animate({left: "0px"}, menuSpeed);
 			container.animate({left: menuWidth}, menuSpeed);
 			//css class to add pushy capability
 			push.animate({left: menuWidth}, menuSpeed);
 		}else{
-			body.addClass(pushyOpenRight);
+			body.classList.contains(pushyOpenRight);
 			pushy.animate({right: '0px'}, menuSpeed);
 			container.animate({right: menuWidth}, menuSpeed);
 			push.animate({right: menuWidth}, menuSpeed);
@@ -94,14 +98,14 @@
 
 	function closePushyFallback(){
 		//animate menu position based on CSS class
-		if( pushy.hasClass(pushyLeft) ){
-			body.removeClass(pushyOpenLeft);
+		if( pushy.classList.contains(pushyLeft) ){
+			body.classList.remove(pushyOpenLeft);
 			pushy.animate({left: "-" + menuWidth}, menuSpeed);
 			container.animate({left: "0px"}, menuSpeed);
 			//css class to add pushy capability
 			push.animate({left: "0px"}, menuSpeed);
 		}else{
-			body.removeClass(pushyOpenRight);
+			body.classList.remove(pushyOpenRight);
 			pushy.animate({right: "-" + menuWidth}, menuSpeed);
 			container.animate({right: "0px"}, menuSpeed);
 			push.animate({right: "0px"}, menuSpeed);
@@ -109,22 +113,38 @@
 	}
 
 	function toggleSubmenu(){
+		
+		let subMenuItems = document.querySelectorAll(submenuClass);
+
 		//hide submenu by default
-		$(submenuClass).addClass(submenuClosedClass);
+		subMenuItems.forEach(function(el){
+			el.classList.add(submenuClosedClass);
 
-		$(submenuClass).on('click', function(){
-	        var selected = $(this);
+			el.addEventListener('click', function(e){
+				
+				var selected = '';
 
-	        if( selected.hasClass(submenuClosedClass) ) {
-	            //hide opened submenus
-	            $(submenuClass).addClass(submenuClosedClass).removeClass(submenuOpenClass);
-	            //show submenu
-	            selected.removeClass(submenuClosedClass).addClass(submenuOpenClass);
-	        }else{
-	            //hide submenu
-	            selected.addClass(submenuClosedClass).removeClass(submenuOpenClass);
-	        }
-	    });
+				if( selected.classList.contains(submenuClosedClass) ) {
+						//hide opened submenus
+						let subMenuItems = document.querySelectorAll(submenuClass);
+						subMenuItems.forEach(function(el) {
+							el.classList.add(submenuClosedClass);
+							el.classList.remove(submenuOpenClass);
+						});
+						//show submenu
+						selected.classList.remove(submenuClosedClass)
+						selected.classList.add(submenuOpenClass);
+				}else{
+						//hide submenu
+						selected.classList.add(submenuClosedClass)
+						selected.classList.remove(submenuOpenClass);
+				}
+			});
+		});
+
+		// subMenuItems.forEach(function(el){
+			
+		// });
 	}
 
 	//checks if 3d transforms are supported removing the modernizr dependency
@@ -163,26 +183,29 @@
 		toggleSubmenu();
 
 		//toggle menu
-		menuBtn.on('click', function(){
-			togglePushy();
+		menuBtn.forEach(function(el) {
+			el.addEventListener('click', function(){
+				togglePushy();
+			});
 		});
+
 		//close menu when clicking site overlay
-		siteOverlay.on('click', function(){
+		siteOverlay.addEventListener('click', function(){
 			togglePushy();
 		});
 	}else{
 		//add css class to body
-		body.addClass('no-csstransforms3d');
+		body.classList.add('no-csstransforms3d');
 
 		//hide menu by default
-		if( pushy.hasClass(pushyLeft) ){
-			pushy.css({left: "-" + menuWidth});
+		if( pushy.classList.contains(pushyLeft) ){
+			pushy.style.left = "-" + menuWidth;
 		}else{
-			pushy.css({right: "-" + menuWidth});
+			pushy.style.right = "-" + menuWidth;
 		}
 
 		//fixes IE scrollbar issue
-		container.css({"overflow-x": "hidden"});
+		container.style.overflowX = "hidden";
 
 		//keep track of menu state (open/close)
 		var opened = false;
@@ -191,18 +214,20 @@
 		toggleSubmenu();
 
 		//toggle menu
-		menuBtn.on('click', function(){
-			if (opened) {
-				closePushyFallback();
-				opened = false;
-			} else {
-				openPushyFallback();
-				opened = true;
-			}
+		menuBtn.forEach(function(el) {
+			addEventListener('click', function(){
+				if (opened) {
+					closePushyFallback();
+					opened = false;
+				} else {
+					openPushyFallback();
+					opened = true;
+				}
+			});
 		});
 
 		//close menu when clicking site overlay
-		siteOverlay.on('click', function(){
+		siteOverlay.addEventListener('click', function(){
 			if (opened) {
 				closePushyFallback();
 				opened = false;
@@ -212,4 +237,6 @@
 			}
 		});
 	}
+
+
 }(jQuery));
